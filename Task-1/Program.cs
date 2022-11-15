fef2a9cb0565a2079c94a7392ba3fdd3cf495737
Игорь <mc_grove@bk.ru>
2022-11-15 16:14:14 -0500

    add Task-1

--- Task-1/Program.cs
+++ Task-1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа. 
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = OutputThreeDigit(number);
+
+int OutputThreeDigit(int num)
+{
+    int res = (num % 100) / 10;
+    return res;
+}
+
+Console.WriteLine(result);
