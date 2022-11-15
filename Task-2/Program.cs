dc07aee431f3fb75df2ed3a87eaf99b139886a9d
Игорь <mc_grove@bk.ru>
2022-11-15 17:20:22 -0500

    add Task-2

--- Task-2/Program.cs
+++ Task-2/Program.cs
@@ -6,3 +6,23 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.WriteLine("Введите любое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (TheThirdDigit(number) == 0) Console.WriteLine("Третьей цифры нет");
+else Console.WriteLine($"Третья цифра в {number} -> {TheThirdDigit(number)}");
+
+
+int TheThirdDigit(int num)
+{
+    int res = 0;
+    if (num >= 100)
+    {
+        while (num > 999)
+        {
+            num /= 10;
+        }
+        res = num % 10;
+    }
+    return res;
+}
